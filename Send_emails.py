f556b9e63d2294fcae61ed3818a2e6ce63170619
LHH-James <liuhuihui520@yahoo.com>
2017-09-14 16:29:56 +0800

    Update Send_emails.py

--- Send_emails.py
+++ Send_emails.py
@@ -8,7 +8,7 @@ receiver = 'xxxxx@qq.com'
 subject = '回家收拾行李通知,测试邮件发送 python,来自LHH-James的问候'
 smtpserver = 'smtp.163.com'
 username = '123456@163.com'
-password = '******'  # 是授权密码，而不是登录密码
+password = '******'  # （网上说：是授权密码，而不是登录密码；但是我测试的时候用的是登录密码）
 msg = MIMEText('晚上记得收拾好东西,明天回家东西别漏了哈！python  测试邮件发送,看到没有，看到没有，看到记得回复哈。。。。。。 看到记得回复哈. 看到记得回复哈!!!!','plain', 'utf-8')  # 中文需参数‘utf-8’，单字节字符不需要
 
 msg['Subject'] = Header(subject, 'utf-8')
